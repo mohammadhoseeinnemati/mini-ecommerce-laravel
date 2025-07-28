79d466f93872792a53ab8cb4689c67601ce93e12
mohammad <mohammadhoseeinnemati1384@gmail.com>
2025-07-28 17:32:23 +0330

    add login and register forms

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <!-- Slider -->
+    <div class="px-3 lg:container group w-full mt-4 lg:mt-10">
+        <div dir="rtl" class="swiper header-slider h-52 md:h-96 cursor-pointer">
+            <div class="swiper-wrapper">
+                <a href="shop.html" class="swiper-slide">
+                    <img src="./assets/images/slider/4.webp" class="rounded-xl" alt="">
+                </a >
+                <a href="shop.html" class="swiper-slide">
+                    <img src="./assets/images/slider/1.jpg" class="rounded-xl" alt="">
+                </a >
+                <a href="shop.html" class="swiper-slide">
+                    <img src="./assets/images/slider/2.jpg" class="rounded-xl" alt="">
+                </a >
+                <a href="shop.html" class="swiper-slide">
+                    <img src="./assets/images/slider/3.gif" class="rounded-xl" alt="">
+                </a >
+            </div>
+            <div class="swiper-pagination-wrapper">
+                <div class="swiper-pagination"></div>
+            </div>
+
+            <!-- Swiper Navigation -->
+            <div
+                class="absolute z-10 bottom-5 opacity-0 invisible group-hover:opacity-100 transition-all duration-300 group-hover:visible right-6 hidden lg:flex items-center gap-x-2 child:flex-center child:w-9 child:h-9 child:cursor-pointer child:bg-white child:dark:bg-gray-800 child:text-gray-700 child:dark:text-gray-200 child:rounded-full child:shadow child-hover:text-blue-600 child-hover:dark:text-blue-500">
+                <button class="button-prev">
+                    <svg class="size-5 -rotate-90">
+                        <use href="#chevron" />
+                    </svg>
+                </button>
+                <button class="button-next">
+                    <svg class="size-5 rotate-90">
+                        <use href="#chevron" />
+                    </svg>
+                </button>
+            </div>
+
+        </div>
+    </div>
+
     <!-- Main -->
     <main class="relative">
         <!-- CATEGORY -->
@@ -605,18 +645,6 @@
             </div>
         </section>
 
-        <!-- BANNER -->
-        <section
-            class="mx-4 lg:container mt-10 lg:mt-20 flex flex-col lg:flex-row items-center gap-5 child:rounded-xl child:overflow-hidden">
-            <a href="shop.html" class="group">
-                <img src="./assets/images/banner/1.webp" class="group-hover:scale-105 transition-transform duration-300"
-                     alt="">
-            </a>
-            <a href="shop.html" class="group">
-                <img src="./assets/images/banner/2.webp" class="group-hover:scale-105 transition-transform duration-300"
-                     alt="">
-            </a>
-        </section>
 
         <!-- Best-selling products -->
         <section class="mx-4 lg:container mt-10 lg:mt-20">
